4083e161a24acfb0e19e4dfb1dd06b00212edc55
Brama Udi <bramaudi@users.noreply.github.com>
2021-02-26 07:47:45 +0700

    Add lazy load image

---
+++ static/js/lazy-image.js
@@ -0,0 +1,24 @@
+var imgs = document.getElementsByClassName('lazy-image')
+
+function lazyImage(target) {
+  var io = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      console.log('X');
+      if (entry.isIntersecting) {
+        var img = entry.target
+        var src = img.getAttribute('data-src')
+
+        img.setAttribute('src', src)
+        img.classList.add('fade-in')
+
+        observer.disconnect()
+      }
+    })
+  })
+
+  io.observe(target)
+}
+
+for (var img of imgs) {
+  lazyImage(img)
+}
