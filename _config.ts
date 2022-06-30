b1d1b75dd450ba8d6ae679db1406017397492a6d
Brama Udi <bramaudi.al@gmail.com>
2022-06-30 20:57:05 +0700

    Major refactor

--- _config.ts
+++ _config.ts
@@ -2,7 +2,7 @@ import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import postcss from "lume/plugins/postcss.ts";
 import terser from "lume/plugins/terser.ts";
-import codeHighlight from "lume/plugins/code_highlight.ts";
+import prism from "lume/plugins/prism.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
@@ -10,16 +10,16 @@ import netlifyCMS from "lume/plugins/netlify_cms.ts";
 import gpm from "https://deno.land/x/gpm@v0.4.1/mod.ts";
 
 const site = lume({
-  location: new URL("https://example.com/"),
+  location: new URL("https://bramaudi.dev/"),
 });
 
 site
   .ignore("README.md")
-  .copy("img")
+  .copy("static")
   .use(postcss())
   .use(terser())
   .use(date())
-  .use(codeHighlight())
+  .use(prism())
   .use(basePath())
   .use(slugifyUrls({ alphanumeric: false }))
   .use(resolveUrls())
